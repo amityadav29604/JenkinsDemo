e71e7f783230cea087f1939909257db9b54cfe51
amityadav29605 <amityadav29604@gmail.com>
2022-11-11 22:57:46 +0530

    first commit

---
+++ src/main/java/com/example/Demo/JenkinDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinDemoApplication.class, args);
+		System.out.println("hello wolrd");
+	}
+
+}
